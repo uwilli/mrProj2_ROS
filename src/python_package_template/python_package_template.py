c8b68febf3c4bb40c71df63672a8c401ad9dddd9
Urban Willi <urban.willi@stud.fhgr.ch>
2022-06-08 16:19:52 +0200

    Added a template for python packages

---
+++ src/python_package_template/python_package_template.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+import roslib
+import rospy
+
+
+if __name__=="__main__":
+    rospy.init_node('python_package_template')
+    rospy.loginfo("Hello world python_package_template")
