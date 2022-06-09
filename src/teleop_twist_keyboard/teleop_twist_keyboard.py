ee952c8ed6e7cebc40acf8fdbe3ad33395bb0097
Urban Willi <urban.willi@stud.fhgr.ch>
2022-06-09 20:26:24 +0200

    Changed .py file arguments from command line to launch file

--- src/teleop_twist_keyboard/teleop_twist_keyboard.py
+++ src/teleop_twist_keyboard/teleop_twist_keyboard.py
@@ -168,8 +168,8 @@ if __name__=="__main__":
 
     speed = rospy.get_param("~speed", 0.5)
     turn = rospy.get_param("~turn", 1.0)
-    repeat = rospy.get_param("~repeat_rate", 0.0)
-    key_timeout = rospy.get_param("~key_timeout", 0.0)
+    repeat = rospy.get_param("/repeat_rate", 0.0)
+    key_timeout = rospy.get_param("/key_timeout", 0.0)
     if key_timeout == 0.0:
         key_timeout = None
 
